31780ab0097e6bb5ab59a1cfec6fe19bc441244f
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-01-26 08:09:18 -0500

    Add model method to compare input password with encryted password

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -25,6 +25,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
