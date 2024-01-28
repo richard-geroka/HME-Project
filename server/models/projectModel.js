bc92ccb81bdf646dd0111ce9d95cb73c6ba6332f
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-01-28 06:43:44 -0500

    Create project model, user can add projects to show to clients

---
+++ server/models/projectModel.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const projectSchema = new mongoose.Schema({
+  projectName: {
+    type: String,
+    required: [true, 'Please provide a project name'],
+  },
+  location: {
+    type: String,
+    required: [true, 'Please provide a project location'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Please provide a short description about the project'],
+  },
+  dateStarted: {
+    type: Date,
+    required: [true, 'Please provide date started'],
+  },
+  dateCompleted: {
+    type: Date,
+    required: [true, 'Please provide date completed'],
+  },
+  photos: [String],
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+export default Project;
