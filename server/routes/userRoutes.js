36f635a0256f6918fc58246023de71714d7a30d7
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-01-28 06:05:01 -0500

    Fix Err_Module_Not_Found, missing .js extension

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import { login, signup } from '../controllers/authController';
+import { login, signup } from '../controllers/authController.js';
 
 const router = express.Router();
 
