66dad364484c8c15d6a383efc6d7b627f1ae47fd
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-01-29 02:45:03 -0500

    Create function to handle adding a service by user

---
+++ server/controllers/serviceController.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/extensions */
+import catchAsync from '../utils/catchAsync.js';
+import AppError from '../utils/appError.js';
+import Service from '../models/serviceModel.js';
+
+const addService = catchAsync(async (req, res, next) => {
+  const service = await Service.create({
+    service: req.body.service,
+    description: req.body.description,
+    photo: req.body.photo,
+  });
+
+  res.status(201).json({
+    status: 'success',
+    message: 'Service added successfully!',
+    service,
+  });
+});
