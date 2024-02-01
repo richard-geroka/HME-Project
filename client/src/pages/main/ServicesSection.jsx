6b880ca4a907fed8e45358a4a5fd6f8ae63b5e46
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-01 18:08:34 -0500

    Use grid system on service section cards and update images height

--- client/src/pages/main/ServicesSection.jsx
+++ client/src/pages/main/ServicesSection.jsx
@@ -1,61 +1,77 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import design from '../../assets/images/design.png';
-import build from '../../assets/images/build.png';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import design from '../../assets/images/design2.png';
+import build from '../../assets/images/build2.png';
 import plan from '../../assets/images/3dPlan.png';
-import permit from '../../assets/images/drawingService.png';
+import permit from '../../assets/images/drawingService2.png';
 
 const ServicesSection = () => {
   return (
     <>
       <section className="services" id="#services">
-        <h2>What we do</h2>
         <Container>
-          <Card style={{ width: '20rem' }}>
-            <Card.Img variant="top" src={design} />
-            <Card.Body>
-              <Card.Title>Design</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the cards content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '20rem' }}>
-            <Card.Img variant="top" src={build} />
-            <Card.Body>
-              <Card.Title>Build</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the cards content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '20rem' }}>
-            <Card.Img variant="top" src={plan} />
-            <Card.Body>
-              <Card.Title>3D Plan</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the cards content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '20rem' }}>
-            <Card.Img variant="top" src={permit} />
-            <Card.Body>
-              <Card.Title>Permit Processing</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the cards content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+          <h2>What we do</h2>
+          <Container fluid>
+            <Row>
+              <Col>
+                <Card style={{ width: '20rem' }}>
+                  <Card.Img variant="top" src={design} />
+                  <Card.Body>
+                    <Card.Title>Design</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the cards content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: '20rem' }}>
+                  <Card.Img variant="top" src={build} />
+                  <Card.Body>
+                    <Card.Title>Build</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the cards content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Card style={{ width: '20rem' }}>
+                  <Card.Img variant="top" src={plan} />
+                  <Card.Body>
+                    <Card.Title>3D Plan</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the cards content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: '20rem' }}>
+                  <Card.Img variant="top" src={permit} />
+                  <Card.Body>
+                    <Card.Title>Permit Processing</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the cards content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
         </Container>
       </section>
     </>
