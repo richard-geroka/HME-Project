2c8394f30b267c6ce891dedbdf8d39b6f3c00f62
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-01 10:34:41 -0500

    Add container component to wrap all cards for styling

--- client/src/pages/main/ServicesSection.jsx
+++ client/src/pages/main/ServicesSection.jsx
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 import design from '../../assets/images/design.png';
 import build from '../../assets/images/build.png';
 import plan from '../../assets/images/3dPlan.png';
@@ -10,50 +11,52 @@ const ServicesSection = () => {
     <>
       <section className="services" id="#services">
         <h2>What we do</h2>
-        <Card style={{ width: '20rem' }}>
-          <Card.Img variant="top" src={design} />
-          <Card.Body>
-            <Card.Title>Design</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the cards content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '20rem' }}>
-          <Card.Img variant="top" src={build} />
-          <Card.Body>
-            <Card.Title>Build</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the cards content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '20rem' }}>
-          <Card.Img variant="top" src={plan} />
-          <Card.Body>
-            <Card.Title>3D Plan</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the cards content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '20rem' }}>
-          <Card.Img variant="top" src={permit} />
-          <Card.Body>
-            <Card.Title>Permit Processing</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the cards content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        <Container>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={design} />
+            <Card.Body>
+              <Card.Title>Design</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={build} />
+            <Card.Body>
+              <Card.Title>Build</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={plan} />
+            <Card.Body>
+              <Card.Title>3D Plan</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={permit} />
+            <Card.Body>
+              <Card.Title>Permit Processing</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Container>
       </section>
     </>
   );
