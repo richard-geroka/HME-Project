52c60ecac2c800b5831934cd5d35c8efe70cd960
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-01 10:20:32 -0500

    Add card component with image for 3D plan service

--- client/src/pages/main/ServicesSection.jsx
+++ client/src/pages/main/ServicesSection.jsx
@@ -2,6 +2,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import design from '../../assets/images/design.png';
 import build from '../../assets/images/build.png';
+import plan from '../../assets/images/3dPlan.png';
 
 const ServicesSection = () => {
   return (
@@ -23,6 +24,17 @@ const ServicesSection = () => {
           <Card.Img variant="top" src={build} />
           <Card.Body>
             <Card.Title>Build</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the cards content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src={plan} />
+          <Card.Body>
+            <Card.Title>3D Plan</Card.Title>
             <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the cards content.
