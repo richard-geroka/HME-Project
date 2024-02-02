f44b88dc0180a8c4cf20f37620c8b63994288e29
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-02 09:59:45 -0500

    Style services cards and call to action to match color theme

--- client/src/pages/main/ServicesSection.jsx
+++ client/src/pages/main/ServicesSection.jsx
@@ -1,9 +1,9 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import design from '../../assets/images/plan-card1.png';
-import build from '../../assets/images/build-card1.png';
-import plan from '../../assets/images/3D-card1.png';
-import permit from '../../assets/images/drawing-card1.png';
+import design from '../../assets/images/design-card5.png';
+import build from '../../assets/images/build-card5.png';
+import plan from '../../assets/images/3D-card5.png';
+import permit from '../../assets/images/drawing-card5.png';
 
 const ServicesSection = () => {
   return (
@@ -14,12 +14,10 @@ const ServicesSection = () => {
         </h2> */}
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
           <div className="col">
-            <Card>
+            <Card bg="success">
               <Card.Img variant="top" src={design} />
-              <Card.Body>
-                <Card.Title className="services-cards-titles">
-                  Design
-                </Card.Title>
+              <Card.Body className="text-light">
+                <Card.Title>Design</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the cards content.
@@ -28,9 +26,9 @@ const ServicesSection = () => {
             </Card>
           </div>
           <div className="col">
-            <Card>
+            <Card bg="success">
               <Card.Img variant="top" src={build} />
-              <Card.Body>
+              <Card.Body className="text-light">
                 <Card.Title>Build</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
@@ -40,9 +38,9 @@ const ServicesSection = () => {
             </Card>
           </div>
           <div className="col">
-            <Card>
+            <Card bg="success">
               <Card.Img variant="top" src={plan} />
-              <Card.Body>
+              <Card.Body className="text-light">
                 <Card.Title>3D Plan</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
@@ -52,9 +50,9 @@ const ServicesSection = () => {
             </Card>
           </div>
           <div className="col">
-            <Card>
+            <Card bg="success">
               <Card.Img variant="top" src={permit} />
-              <Card.Body>
+              <Card.Body className="text-light">
                 <Card.Title>Permit Processing</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
@@ -64,17 +62,12 @@ const ServicesSection = () => {
             </Card>
           </div>
         </div>
-        <p className="services-p mt-5">
-          <Button
-            variant="outline-success"
-            href="#contact"
-            size="lg"
-            className="me-1"
-          >
-            Contact us
+        <p className="services-p mt-5">OUR PROFESSIONAL SERVICES</p>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <Button variant="success" size="lg" href="#contact">
+            get a quote
           </Button>
-          to learn more about our professional services.
-        </p>
+        </div>
       </section>
     </>
   );
