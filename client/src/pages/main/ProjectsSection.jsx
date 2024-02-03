e57c8b482b1c95de4170bbfe809c92647831b6c2
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-02 23:10:27 -0500

    Add carousel to projects section

--- client/src/pages/main/ProjectsSection.jsx
+++ client/src/pages/main/ProjectsSection.jsx
@@ -1,7 +1,42 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
+import design from '../../assets/images/carousel-test.png';
+
 const ProjectsSection = () => {
   return (
     <>
-      <div>Hello, World</div>
+      <section className="projects" id="projects">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <Carousel
+            fade
+            className="border"
+            style={{ width: '50rem' }}
+            data-bs-theme="dark"
+          >
+            <Carousel.Item>
+              <Image src={design} />
+              <Carousel.Caption>
+                <h3 className="text-success">Photoism</h3>
+                <p className="text-success">SM Lanang</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image src={design} />
+              <Carousel.Caption>
+                <h3 className="text-success">Photoism</h3>
+                <p className="text-success">SM Lanang</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image src={design} />
+              <Carousel.Caption>
+                <h3 className="text-success">Photoism</h3>
+                <p className="text-success">SM Lanang</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </section>
     </>
   );
 };
