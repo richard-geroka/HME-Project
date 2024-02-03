31b9cd4562e16e54aaaf915bfde76ecf5fc1558f
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-02 23:56:01 -0500

    Style projects section with gradient background color and resize carousel

--- client/src/pages/main/ProjectsSection.jsx
+++ client/src/pages/main/ProjectsSection.jsx
@@ -7,28 +7,23 @@ const ProjectsSection = () => {
     <>
       <section className="projects" id="projects">
         <div className="d-flex flex-column justify-content-center align-items-center">
-          <Carousel
-            fade
-            className="border"
-            style={{ width: '50rem' }}
-            data-bs-theme="dark"
-          >
+          <Carousel slide={false} data-bs-theme="dark">
             <Carousel.Item>
-              <Image src={design} />
+              <Image src={design} fluid rounded />
               <Carousel.Caption>
                 <h3 className="text-success">Photoism</h3>
                 <p className="text-success">SM Lanang</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Image src={design} />
+              <Image src={design} fluid rounded />
               <Carousel.Caption>
                 <h3 className="text-success">Photoism</h3>
                 <p className="text-success">SM Lanang</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Image src={design} />
+              <Image src={design} fluid rounded />
               <Carousel.Caption>
                 <h3 className="text-success">Photoism</h3>
                 <p className="text-success">SM Lanang</p>
