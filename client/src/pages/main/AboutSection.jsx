d413d57123a164bdf8f97d12aa1b11268242ec21
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-06 09:54:44 -0500

    Update about us content to be responsive on different devices, add background color

--- client/src/pages/main/AboutSection.jsx
+++ client/src/pages/main/AboutSection.jsx
@@ -1,17 +1,15 @@
-import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
 import teampic from '../../assets/images/hmeteam.png';
 
 const AboutSection = () => {
   return (
     <>
       <section className="about" id="about">
-        <div className="row row-cols-1 row-cols-lg-2">
-          <div className="col">
-            <Image src={teampic} fluid />
+        <h2 className="mb-3">Who we are and what we do -</h2>
+        <div className="about-container-grid">
+          <div className="col-image">
+            <img src={teampic} alt="HME team picture" />
           </div>
-          <div className="col about-text">
-            <h2 className="mb-3">Who we are and what we do -</h2>
+          <div className="col-about-text">
             <p>
               We are a team of architects, engineers and builders. We will build
               your dream into reality.
@@ -20,9 +18,12 @@ const AboutSection = () => {
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
               deleniti possimus saepe animi dolor dicta magni modi.
             </p>
-            <Button variant="danger" size="lg" className="mt-4 mb-3">
-              Contact Us
-            </Button>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+              unde delectus quos molestiae autem laboriosam culpa
+              exercitationem.
+            </p>
+            <button className="about-btn">Contact us</button>
           </div>
         </div>
       </section>
