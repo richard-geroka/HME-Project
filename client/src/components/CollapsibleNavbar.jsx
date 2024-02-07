6ca45588622eb7277cce1cbe6e08d48d5bfab702
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-07 04:02:47 -0500

    Add logo to navbar

--- client/src/components/CollapsibleNavbar.jsx
+++ client/src/components/CollapsibleNavbar.jsx
@@ -1,26 +1,39 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/images/logo.png';
 
 const CollapsibleNavbar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" fixed="top" variant="light" bg="light">
       <Container>
-        <Navbar.Brand href="#home">HME Construction Services</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            alt="hme logo"
+            style={{ width: '120px', height: 'auto' }}
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Container>
             <Nav className="d-flex justify-content-end">
-              <Nav.Link href="#services" className="fw-normal text-success">
+              <Nav.Link
+                href="#services"
+                className="fw-normal fs-5 text-success"
+              >
                 Services
               </Nav.Link>
-              <Nav.Link href="#projects" className="fw-normal text-success">
+              <Nav.Link
+                href="#projects"
+                className="fw-normal fs-5 text-success"
+              >
                 Projects
               </Nav.Link>
-              <Nav.Link href="#about" className="fw-normal text-success">
+              <Nav.Link href="#about" className="fw-normal fs-5 text-success">
                 About Us
               </Nav.Link>
-              <Nav.Link href="#contact" className="text-danger fw-bolder">
+              <Nav.Link href="#contact" className="text-danger fs-5 fw-bolder">
                 Contact Us
               </Nav.Link>
             </Nav>
