a09667e2a22f2f912bd2e7ad1f2b4801faebb930
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-07 03:20:05 -0500

    Style button components for hero section

--- client/src/pages/main/HeroSection.jsx
+++ client/src/pages/main/HeroSection.jsx
@@ -4,15 +4,17 @@ const HeroSection = () => {
   return (
     <>
       <section className="hero" id="home">
-        <h1>
-          HME <em>Construction Services</em>
-        </h1>
-        <p>
-          <span className="text-plan">Plan.</span>{' '}
-          <span className="text-design">Design.</span>{' '}
-          <span className="text-build">Build.</span>
-        </p>
-        <p className="hero-dream">Your dream into reality</p>
+        <div className="hero-text-content">
+          <h1>
+            HME <em>Construction Services</em>
+          </h1>
+          <p>
+            <span className="text-plan">Plan.</span>{' '}
+            <span className="text-design">Design.</span>{' '}
+            <span className="text-build">Build.</span>
+          </p>
+          <p className="hero-dream">Your dream into reality</p>
+        </div>
         <HoverButtonLink
           link={'#projects'}
           content={'Learn more'}
