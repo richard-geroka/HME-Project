e3f548b89854b0fd5400d6fa8ac666f28a6b624a
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-09 04:00:11 -0500

    Add button get a quote button to service page for call to action

--- client/src/pages/services/ServicePage.jsx
+++ client/src/pages/services/ServicePage.jsx
@@ -1,5 +1,6 @@
 import CollapsibleNavbar from '../../components/CollapsibleNavbar';
 import Footer from '../../components/Footer';
+import HoverButtonLink from '../../components/HoverButtonLink';
 import ServiceCards from '../../components/ServiceCards';
 import ServiceHero from './ServiceHero';
 
@@ -15,6 +16,13 @@ const ServicePage = () => {
         <div className="wrapper">
           <ServiceHero />
           <ServiceCards isMainPage={'false'} />
+          <div className="d-flex flex-column justify-content-center align-items-center my-3 mt-lg-5">
+            <HoverButtonLink
+              className={'hero-btn'}
+              link={'/contact'}
+              content={'Get a quote'}
+            />
+          </div>
         </div>
       </main>
       <Footer />
