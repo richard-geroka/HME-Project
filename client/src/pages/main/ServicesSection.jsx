65655ef030c4051998f0afe18852d2b84f6cb1c6
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-09 02:56:53 -0500

    Update image on build card

--- client/src/pages/main/ServicesSection.jsx
+++ client/src/pages/main/ServicesSection.jsx
@@ -9,7 +9,7 @@ const ServicesSection = () => {
           <h2 className="services-h2 my-4 text-success text-center">
             Our professional services -
           </h2>
-          <ServiceCards />
+          <ServiceCards isMainPage={'true'} />
           <div className="d-flex flex-column justify-content-center align-items-center my-3 mt-lg-5">
             <HoverButtonLink
               className={'hero-btn'}
