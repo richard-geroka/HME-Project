f12a1ccff6f7735d8c455a4a91563b94b45b4976
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-09 09:44:56 -0500

    Add image for why our service section

--- client/src/pages/services/WhyOurServices.jsx
+++ client/src/pages/services/WhyOurServices.jsx
@@ -1,7 +1,17 @@
+import handshake from '../../assets/images/handshake.png';
+
 const WhyOurServices = () => {
   return (
     <>
-      <div>Hello, World!</div>
+      <div className="our-services-container">
+        <h2>Why choose our services ?</h2>
+        <div className="our-services-grid">
+          <div className="services-image-col">
+            <img src={handshake} alt="People shaking hand" />
+          </div>
+          <div className="services-text-col"></div>
+        </div>
+      </div>
     </>
   );
 };
