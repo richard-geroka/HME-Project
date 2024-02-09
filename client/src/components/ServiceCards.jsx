223acae287af9fa3a9a02a9d2ac4bd30b52c0e9b
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-09 02:10:02 -0500

    Update service cards components to be refactored and reuse in other components

--- client/src/components/ServiceCards.jsx
+++ client/src/components/ServiceCards.jsx
@@ -1,7 +1,70 @@
-const ServiceCards = () => {
+/* eslint-disable react/prop-types */
+import Card from 'react-bootstrap/Card';
+import design from '../assets/images/design-card7.png';
+import build from '../assets/images/build-card7.png';
+import plan from '../assets/images/3D-card7.png';
+import permit from '../assets/images/drawing-card7.png';
+
+const ServiceCards = ({ isMainPage }) => {
+  let isMain;
+
+  if (isMainPage === 'true') {
+    isMain = true;
+  } else {
+    isMain = false;
+  }
   return (
     <>
-      <div>Hello, World!</div>
+      <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4 g-4">
+        <div className="col">
+          <Card bg="success">
+            <Card.Img variant="top" src={design} alt="blue print design" />
+            <Card.Body className="text-light">
+              <Card.Title>Design</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col">
+          <Card bg="success">
+            <Card.Img variant="top" src={build} alt="a building being built" />
+            <Card.Body className="text-light">
+              <Card.Title>Build</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col">
+          <Card bg="success">
+            <Card.Img variant="top" src={plan} alt="3d plan of a house" />
+            <Card.Body className="text-light">
+              <Card.Title>3D Plan</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col">
+          <Card bg="success">
+            <Card.Img variant="top" src={permit} alt="pencil on top of docs" />
+            <Card.Body className="text-light">
+              <Card.Title>Permit Processing</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
     </>
   );
 };
--- client/src/pages/main/ServicesSection.jsx
+++ client/src/pages/main/ServicesSection.jsx
@@ -1,88 +1,13 @@
-/* eslint-disable react/prop-types */
-import Card from 'react-bootstrap/Card';
-import design from '../../assets/images/design-card7.png';
-import build from '../../assets/images/build-card7.png';
-import plan from '../../assets/images/3D-card7.png';
-import permit from '../../assets/images/drawing-card7.png';
 import HoverButtonLink from '../../components/HoverButtonLink';
 
-const ServicesSection = ({ checkIsMain }) => {
-  let isMain;
-
-  if (checkIsMain === 'true') {
-    isMain = true;
-  } else {
-    isMain = false;
-  }
-
+const ServicesSection = () => {
   return (
     <>
       <section className="services" id="services" aria-label="Services">
         <div className="wrapper">
-          {isMain ? (
-            <h2 className="services-h2 my-4 text-success text-center">
-              Our professional services -
-            </h2>
-          ) : undefined}
-
-          <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4 g-4">
-            <div className="col">
-              <Card bg="success">
-                <Card.Img variant="top" src={design} alt="blue print design" />
-                <Card.Body className="text-light">
-                  <Card.Title>Design</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-            <div className="col">
-              <Card bg="success">
-                <Card.Img
-                  variant="top"
-                  src={build}
-                  alt="a building being built"
-                />
-                <Card.Body className="text-light">
-                  <Card.Title>Build</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-            <div className="col">
-              <Card bg="success">
-                <Card.Img variant="top" src={plan} alt="3d plan of a house" />
-                <Card.Body className="text-light">
-                  <Card.Title>3D Plan</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-            <div className="col">
-              <Card bg="success">
-                <Card.Img
-                  variant="top"
-                  src={permit}
-                  alt="pencil on top of docs"
-                />
-                <Card.Body className="text-light">
-                  <Card.Title>Permit Processing</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the cards content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-          </div>
+          <h2 className="services-h2 my-4 text-success text-center">
+            Our professional services -
+          </h2>
           <div className="d-flex flex-column justify-content-center align-items-center my-3 mt-lg-5">
             <HoverButtonLink
               className={'hero-btn'}
