2306194defb997f1a2c14cf1a0424be7d7095999
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-10 00:56:35 -0500

    Update footer link to navigate to pages

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,27 +4,27 @@ const Footer = () => {
       <footer className="py-3 my-4">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
           <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
+            <a href="/" className="nav-link px-2 text-body-secondary">
               Home
             </a>
           </li>
           <li className="nav-item">
-            <a href="#services" className="nav-link px-2 text-body-secondary">
+            <a href="/services" className="nav-link px-2 text-body-secondary">
               Services
             </a>
           </li>
           <li className="nav-item">
-            <a href="#projects" className="nav-link px-2 text-body-secondary">
+            <a href="/projects" className="nav-link px-2 text-body-secondary">
               Projects
             </a>
           </li>
           <li className="nav-item">
-            <a href="#about" className="nav-link px-2 text-body-secondary">
+            <a href="/about" className="nav-link px-2 text-body-secondary">
               About Us
             </a>
           </li>
           <li className="nav-item">
-            <a href="#contact" className="nav-link px-2 text-body-secondary">
+            <a href="/contact" className="nav-link px-2 text-body-secondary">
               Contact Us
             </a>
           </li>
