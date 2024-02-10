8be2350308bf78ed8d672c5685ee6c8872acb59e
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-10 09:19:51 -0500

    Add text content to our story section on about page

--- client/src/pages/about/OurStory.jsx
+++ client/src/pages/about/OurStory.jsx
@@ -1,7 +1,22 @@
 const OurStory = () => {
   return (
     <>
-      <div>Hello, World!</div>
+      <section className="about-our-story">
+        <div className="wrapper">
+          <h2>Our Story</h2>
+          <div className="our-story-grid">
+            <div className="our-story-image"></div>
+            <div className="our-story-text">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo voluptate distinctio ducimus nobis ipsa quae, nemo
+                rerum ad laudantium dicta vero placeat voluptatum tempore ut cum
+                eos saepe, id odio.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
