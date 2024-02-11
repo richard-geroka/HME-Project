1e3c8f8eed771ffbe3f1774634544266d9f48714
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-11 02:44:01 -0500

    Add contact page route to router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import MainPage from './pages/main/MainPage';
 import ServicePage from './pages/services/ServicePage';
 import ProjectPage from './pages/projects/ProjectPage';
 import AboutPage from './pages/about/AboutPage';
+import ContactPage from './pages/contact/ContactPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/services" element={<ServicePage />} />
           <Route path="/projects" element={<ProjectPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
     </div>
