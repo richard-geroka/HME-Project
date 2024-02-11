46ff6fb7ce5deb5cfa5eb90422b5de2ab2d46c17
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-11 02:15:33 -0500

    Use the images to meet our team section for about page

--- client/src/pages/about/MeetOurTeam.jsx
+++ client/src/pages/about/MeetOurTeam.jsx
@@ -1,3 +1,9 @@
+import architect from '../../assets/images/architect.png';
+import engineer from '../../assets/images/engineer.png';
+import manager from '../../assets/images/manager.png';
+import builder from '../../assets/images/builder.png';
+import otherBuilder from '../../assets/images/builder2.png';
+
 const MeetOurTeam = () => {
   return (
     <>
@@ -6,19 +12,24 @@ const MeetOurTeam = () => {
           <h2>Meet our team</h2>
           <div className="our-team-grid">
             <div className="our-team-col">
+              <img src={architect} alt="HME Architect" />
               <p>Architect</p>
             </div>
             <div className="our-team-col">
+              <img src={engineer} alt="HME engineer" />
               <p>Engineer</p>
             </div>
             <div className="our-team-col">
-              <p>Builder</p>
-            </div>
-            <div className="our-team-col">
-              <p>Builder</p>
-            </div>
-            <div className="our-team-col">
+              <img src={manager} alt="HME manager" />
               <p>Manager</p>
+            </div>
+            <div className="our-team-col">
+              <img src={builder} alt="HME builder" />
+              <p>Builder</p>
+            </div>
+            <div className="our-team-col">
+              <img src={otherBuilder} alt="HME builder" />
+              <p>Builder</p>
             </div>
           </div>
         </div>
