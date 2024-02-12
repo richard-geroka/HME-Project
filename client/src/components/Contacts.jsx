ede6463d4f1e0bf56a7a24a929e0e833c8db9e43
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-11 22:14:53 -0500

    Create Contacts component to refactor contacts info code and style

---
+++ client/src/components/Contacts.jsx
@@ -0,0 +1,38 @@
+const Contacts = () => {
+  return (
+    <>
+      <div className="contact-container-grid">
+        <div className="col-by-phone">
+          <h3>
+            <i className="bi bi-telephone-fill"></i> Phone
+          </h3>
+          <p>Call or text</p>
+          <p>+63 111 222 3333</p>
+        </div>
+        <div className="col-by-socialmedia">
+          <h3>
+            <i className="bi bi-chat-dots-fill"></i> Chat
+          </h3>
+          <p>
+            <a href="#">
+              <i className="bi bi-facebook"></i> facebook page
+            </a>
+          </p>
+          <p>
+            <a href="#">
+              <i className="bi bi-messenger"></i> messenger
+            </a>
+          </p>
+        </div>
+        <div className="col-by-email">
+          <h3>
+            <i className="bi bi-envelope-at-fill"></i> Email
+          </h3>
+          <p>hme@company.com</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contacts;
--- client/src/pages/main/ContactSection.jsx
+++ client/src/pages/main/ContactSection.jsx
@@ -1,39 +1,12 @@
+import Contacts from '../../components/Contacts';
+
 const ContactSection = () => {
   return (
     <>
       <section className="contact" id="contact">
         <div className="wrapper">
           <h2>Get in touch -</h2>
-          <div className="contact-container-grid">
-            <div className="col-by-phone">
-              <h3>
-                <i className="bi bi-telephone-fill"></i> Phone
-              </h3>
-              <p>Call or text</p>
-              <p>+63 111 222 3333</p>
-            </div>
-            <div className="col-by-socialmedia">
-              <h3>
-                <i className="bi bi-chat-dots-fill"></i> Chat
-              </h3>
-              <p>
-                <a href="#">
-                  <i className="bi bi-facebook"></i> facebook page
-                </a>
-              </p>
-              <p>
-                <a href="#">
-                  <i className="bi bi-messenger"></i> messenger
-                </a>
-              </p>
-            </div>
-            <div className="col-by-email">
-              <h3>
-                <i className="bi bi-envelope-at-fill"></i> Email
-              </h3>
-              <p>hme@company.com</p>
-            </div>
-          </div>
+          <Contacts />
         </div>
       </section>
     </>
