e80975c4dee6733f269c4494ac89bf453ac74b4b
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-11 22:31:44 -0500

    Update contact info section spacing for different devices

--- client/src/pages/contact/ContactInfo.jsx
+++ client/src/pages/contact/ContactInfo.jsx
@@ -3,7 +3,7 @@ import Contacts from '../../components/Contacts';
 const ContactInfo = () => {
   return (
     <>
-      <section className="contact">
+      <section className="contact p-2">
         <div className="wrapper">
           <h2>For pricing and other inquiries, please contact us via</h2>
           <Contacts />
