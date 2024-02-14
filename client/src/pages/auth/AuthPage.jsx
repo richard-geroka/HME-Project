027e0568593e0f51b26ad298b6ba96c96be1e8ef
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-14 07:56:35 -0500

    Add a form to auth page to submit username and password

--- client/src/pages/auth/AuthPage.jsx
+++ client/src/pages/auth/AuthPage.jsx
@@ -1,7 +1,19 @@
 const Auth = () => {
   return (
     <>
-      <div>Hello, World!</div>
+      <div className="auth-container">
+        <form>
+          <label htmlFor="username">username</label>
+          <input type="text" id="username" placeholder="username" required />
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="password"
+            required
+          />
+        </form>
+      </div>
     </>
   );
 };
