7e70dd99e949e14841aa8ef9035254e2c437aab5
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-13 21:58:19 -0500

    Update ProjectInfo and prepare it to display project data from database

--- client/src/pages/projects/ProjectInfo.jsx
+++ client/src/pages/projects/ProjectInfo.jsx
@@ -1,7 +1,20 @@
 const ProjectInfo = () => {
   return (
     <>
-      <div>Hello, World!</div>
+      <section className="project-info">
+        <div className="wrapper">
+          <div className="project-info-container">
+            <div className="project-info-col">
+              <img src="" alt="" />
+              <p className="project-info-data"></p>
+              <p className="project-info-data"></p>
+              <p className="project-info-data"></p>
+              <p className="project-info-data"></p>
+              <p className="project-info-data"></p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
