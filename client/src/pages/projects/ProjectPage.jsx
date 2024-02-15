1272b0790c68959b8db910542408aef2265d2067
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-15 04:01:33 -0500

    Add ProjectInfo to ProjectPage to be rendered

--- client/src/pages/projects/ProjectPage.jsx
+++ client/src/pages/projects/ProjectPage.jsx
@@ -1,6 +1,7 @@
 import CollapsibleNavbar from '../../components/CollapsibleNavbar';
 import Footer from '../../components/Footer';
 import ProjectHero from './ProjectHero';
+import ProjectInfo from './ProjectInfo';
 
 const ProjectPage = () => {
   return (
@@ -12,6 +13,7 @@ const ProjectPage = () => {
       </header>
       <main>
         <ProjectHero />
+        <ProjectInfo />
       </main>
       <Footer />
     </>
