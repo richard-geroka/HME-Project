c299052fab3c3023c495e1f8f84daf8f071d716a
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-16 10:30:45 -0500

    Add image and project info content to ProjectInfo

--- client/src/pages/projects/ProjectInfo.jsx
+++ client/src/pages/projects/ProjectInfo.jsx
@@ -9,12 +9,18 @@ const ProjectInfo = () => {
           <ProjectModal />
           <div className="project-info-container">
             <div className="project-info-col">
-              <img src="" alt="" />
-              <p className="project-info-data"></p>
-              <p className="project-info-data"></p>
-              <p className="project-info-data"></p>
-              <p className="project-info-data"></p>
-              <p className="project-info-data"></p>
+              <img
+                src="../../../../server/public/images/projects-65b63748c304d887a531b157-1707809320734-1.jpeg"
+                alt=""
+              />
+              <p className="project-info-data">Project Name: Photoism</p>
+              <p className="project-info-data">Location: SM Lanang</p>
+              <p className="project-info-data">Duration: 3 Months</p>
+              <p className="project-info-data">
+                Description: Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Excepturi laboriosam beatae debitis asperiores
+                perferendis.
+              </p>
             </div>
           </div>
         </div>
