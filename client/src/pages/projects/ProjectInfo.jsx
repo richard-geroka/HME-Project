973d5b92716cb34a23164150c402190fefcb72ac
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-16 11:00:12 -0500

    Style ProjectInfo section on project page

--- client/src/pages/projects/ProjectInfo.jsx
+++ client/src/pages/projects/ProjectInfo.jsx
@@ -1,26 +1,31 @@
 import axios from 'axios';
 import ProjectModal from './ProjectModal';
+import fitoutCard from '../../assets/images/fitout-card7.png';
 
 const ProjectInfo = () => {
   return (
     <>
       <section className="project-info">
         <div className="wrapper">
-          <ProjectModal />
+          <div className="modal-container">
+            <ProjectModal />
+          </div>
           <div className="project-info-container">
             <div className="project-info-col">
-              <img
-                src="../../../../server/public/images/projects-65b63748c304d887a531b157-1707809320734-1.jpeg"
-                alt=""
-              />
-              <p className="project-info-data">Project Name: Photoism</p>
-              <p className="project-info-data">Location: SM Lanang</p>
-              <p className="project-info-data">Duration: 3 Months</p>
-              <p className="project-info-data">
-                Description: Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Excepturi laboriosam beatae debitis asperiores
-                perferendis.
-              </p>
+              <div>
+                <img src={fitoutCard} alt="" />
+              </div>
+              <div className="project-info-text">
+                <p className="project-info-data">Project: Photoism</p>
+                <p className="project-info-data">Location: SM Lanang</p>
+                <p className="project-info-data">Duration: 3 Months</p>
+                <p className="project-info-data">
+                  Description: Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Excepturi laboriosam beatae debitis
+                  asperiores perferendis.
+                </p>
+                <button className="hero-btn">View Photos</button>
+              </div>
             </div>
           </div>
         </div>
