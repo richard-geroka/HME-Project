98bc0a2e50b6e9a93324276dd6dab340154f3531
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-17 02:48:04 -0500

    Use map function on projects and return a styled container for each project

--- client/src/pages/projects/ProjectInfo.jsx
+++ client/src/pages/projects/ProjectInfo.jsx
@@ -26,24 +26,28 @@ const ProjectInfo = () => {
           <div className="modal-container">
             <ProjectModal />
           </div>
-          <div className="project-info-container">
-            <div className="project-info-col">
-              <div>
-                <img src={fitoutCard} alt="" />
+          {projects.map((project) => {
+            return (
+              <div className="project-info-container">
+                <div className="project-info-col">
+                  <div>
+                    <img src={fitoutCard} alt="" />
+                  </div>
+                  <div className="project-info-text">
+                    <p className="project-info-data">Project: Photoism</p>
+                    <p className="project-info-data">Location: SM Lanang</p>
+                    <p className="project-info-data">Duration: 3 Months</p>
+                    <p className="project-info-data">
+                      Description: Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit. Excepturi laboriosam beatae debitis
+                      asperiores perferendis.
+                    </p>
+                    <button className="hero-btn">View Photos</button>
+                  </div>
+                </div>
               </div>
-              <div className="project-info-text">
-                <p className="project-info-data">Project: Photoism</p>
-                <p className="project-info-data">Location: SM Lanang</p>
-                <p className="project-info-data">Duration: 3 Months</p>
-                <p className="project-info-data">
-                  Description: Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Excepturi laboriosam beatae debitis
-                  asperiores perferendis.
-                </p>
-                <button className="hero-btn">View Photos</button>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </section>
     </>
