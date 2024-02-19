388918d9a5e506cf01cfb33d0d92d07c6ad54656
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-19 05:13:05 -0500

    Create form with label and input to UpdateProjectInfo component

--- client/src/pages/projects/UpdateProjectInfo.jsx
+++ client/src/pages/projects/UpdateProjectInfo.jsx
@@ -4,7 +4,60 @@ import Form from 'react-bootstrap/Form';
 const UpdateProjectInfo = () => {
   return (
     <>
-      <div>Hello, World!</div>
+      <Form>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Project Name"
+          className="mb-3"
+        >
+          <Form.Control
+            type="text"
+            placeholder="project name"
+            name="projectName"
+            value={projectInfo.projectName}
+            onChange={handleChange}
+          />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Location"
+          className="mb-3"
+        >
+          <Form.Control
+            type="text"
+            placeholder="Location"
+            name="location"
+            value={projectInfo.location}
+            onChange={handleChange}
+          />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Description"
+          className="mb-3"
+        >
+          <Form.Control
+            type="text"
+            placeholder="description"
+            name="description"
+            value={projectInfo.description}
+            onChange={handleChange}
+          />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Build Duration"
+          className="mb-3"
+        >
+          <Form.Control
+            type="text"
+            placeholder="build duration"
+            name="duration"
+            value={projectInfo.duration}
+            onChange={handleChange}
+          />
+        </FloatingLabel>
+      </Form>
     </>
   );
 };
