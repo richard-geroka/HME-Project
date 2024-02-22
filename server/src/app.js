fc9ad71abf199424e2fe1900635c99082d99962c
Richard Geroka <132794026+richard-geroka@users.noreply.github.com>
2024-02-22 01:16:29 -0500

    Add middleware to server to serve static file

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/extensions */
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
@@ -18,6 +20,11 @@ const app = express();
 app.use(cors());
 
 // Global Middlewares
+
+// Serve static file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set http security
 app.use(helmet());
